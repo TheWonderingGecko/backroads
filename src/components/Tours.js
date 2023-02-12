2c40d1d8f96bca8b694810745ccf55ac97becda5
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-02-12 01:52:12 -0600

    additions

---
+++ src/components/Tours.js
@@ -0,0 +1,54 @@
+import { toursSections } from '../data'
+
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <div className="section-title">
+        <h2>
+          featured <span>tours</span>
+        </h2>
+      </div>
+
+      <div className="section-center featured-center">
+        {toursSections.map(
+          ({
+            id,
+            tours_image,
+            date,
+            title,
+            lorem,
+            country,
+            duration,
+            price,
+          }) => {
+            return (
+              <article className="tour-card" key={id}>
+                <div className="tour-img-container">
+                  <img src={tours_image} className="tour-img" alt="" />
+                  <p className="tour-date">{date}</p>
+                </div>
+                <div className="tour-info">
+                  <div className="tour-title">
+                    <h4>{title}</h4>
+                  </div>
+                  <p>{lorem}</p>
+                  <div className="tour-footer">
+                    <p>
+                      <span>
+                        <i className="fas fa-map"></i>
+                      </span>{' '}
+                      {country}
+                    </p>
+                    <p>{duration}</p>
+                    <p>{price}</p>
+                  </div>
+                </div>
+              </article>
+            )
+          }
+        )}
+      </div>
+    </section>
+  )
+}
+export default Tours
---
+++ src/data.js
@@ -0,0 +1,64 @@
+import tour_1 from '../src/images/tour-1.jpeg'
+import tour_2 from '../src/images/tour-2.jpeg'
+import tour_3 from '../src/images/tour-3.jpeg'
+import tour_4 from '../src/images/tour-4.jpeg'
+export const pageLinks = [
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
+  { id: 5, href: '#tours', text: 'prices' },
+]
+
+export const socialLinks = [
+  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+]
+
+export const toursSections = [
+  {
+    id: 1,
+    tours_image: tour_1,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    lorem:
+      ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    country: ' china',
+    duration: '6 days',
+    price: 'from $2100',
+  },
+  {
+    id: 2,
+    tours_image: tour_2,
+    date: 'October 1st, 2020',
+    title: 'Best of Java',
+    lorem:
+      ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    country: ' indonesia',
+    duration: '11 days',
+    price: 'from $1400',
+  },
+  {
+    id: 3,
+    tours_image: tour_3,
+    date: 'September 15th, 2020',
+    title: 'Explore Hong Kong',
+    lorem:
+      ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    country: ' Honk kong',
+    duration: '8 days',
+    price: 'from $5000',
+  },
+  {
+    id: 4,
+    tours_image: tour_4,
+    date: 'December 5th, 2019',
+    title: 'Kenya Highlands',
+    lorem:
+      ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    country: ' Kenya',
+    duration: '20 days',
+    price: 'from $3300',
+  },
+]
