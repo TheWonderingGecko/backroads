160db49a1204c81b1354322503cdd1825806444a
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-02-15 13:08:01 -0600

    removed

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          <a href="replace" className="btn">
             read more
           </a>
         </article>
